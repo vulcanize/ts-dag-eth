1b50ace93dfc2dde0290f441a8e3482c86b57ea7
i-norden <iansnorden@gmail.com>
2021-09-02 23:29:56 -0500

    state account encode, decode, prepare, and validate

--- state_account/src/index.ts
+++ state_account/src/index.ts
@@ -1,4 +1,29 @@
-import { AccountData } from 'ethereumjs-util'
+import { AccountData, Account as EthAccount } from 'ethereumjs-util'
+import { ByteView } from 'multiformats/codecs/interface.js'
+import { Account as DAGAccount } from './interface.js'
+import { cidFromHash, hashFromCID } from '../../util/src/util.js'
 
 export const name = 'eth-account-snapshot'
 export const code = 0x97
+const rawCode = 0x55
+
+export function encode(node: DAGAccount): ByteView<DAGAccount> {
+    const account = EthAccount.fromAccountData({
+        nonce: node.Nonce,
+        balance: node.Balance,
+        stateRoot: hashFromCID(node.StorageRootCID),
+        codeHash: hashFromCID(node.CodeCID),
+    })
+    return account.serialize()
+}
+
+export function decode(bytes: ByteView<DAGAccount>): DAGAccount {
+    const bytesBuffer = new Buffer(bytes.valueOf())
+    const ethAccount = EthAccount.fromRlpSerializedAccount(bytesBuffer)
+    return {
+        Nonce: ethAccount.nonce,
+        Balance: ethAccount.balance,
+        StorageRootCID: cidFromHash(code, ethAccount.stateRoot),
+        CodeCID: cidFromHash(rawCode, ethAccount.codeHash)
+    }
+}
--- state_account/src/interface.ts
+++ state_account/src/interface.ts
@@ -1,15 +1,8 @@
 import { CID } from "multiformats/cid";
 
 export interface Account {
-    Nonce: BigInteger,
-    Balance: BigInteger,
+    Nonce: bigint,
+    Balance: bigint,
     StorageRootCID: CID,
     CodeCID: CID
-}
-
-export interface RawAccount {
-    Nonce: BigInteger,
-    Balance: BigInteger,
-    StorageRootCID: Uint8Array,
-    CodeCID: Uint8Array
 }
