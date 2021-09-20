4e91947e62c20c76b12403b13aa5495d8addef82
i-norden <iansnorden@gmail.com>
2021-09-20 09:47:36 -0500

    Log type guard

--- log/src/interface.ts
+++ log/src/interface.ts
@@ -11,3 +11,17 @@ export type LogBuffer = [Buffer, Buffer[], Buffer]
 export type Logs = Log[]
 
 export type LogsBuffer = LogBuffer[]
+
+export function isLog (x: any): x is Log {
+  if ((x as Log).Address === undefined) {
+    return false
+  }
+  if ((x as Log).Topics === undefined) {
+    return false
+  }
+  if ((x as Log).Data === undefined) {
+    return false
+  }
+
+  return true
+}
